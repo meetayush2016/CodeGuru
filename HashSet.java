96afd626bdbe9bc0e2756875dcce57dce93da3fc
Ayush Sharma <83689656+meetayush2016@users.noreply.github.com>
2023-06-26 20:30:11 +0530

    Create  HashSet.java

---
+++ HashSet.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Hello{
+    public static void main(String[] arga){
+        
+        //creating a hash set
+        HashSet<Integer> set = new HashSet<>();
+        
+        //adding values in hash set
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        
+        //searching
+        if(set.contains(1)){
+            System.out.println("Set contains 1");
+        }
+        if(!set.contains(9)){
+            System.out.println("Set contains 1");
+        }
+        
+        
+        
+        
+      
+        
+        
+    }
+}
