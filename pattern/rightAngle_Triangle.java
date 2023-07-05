9d4a7fb5d5d4caef257713d6edbec898886db28d
Ayush Sharma <83689656+meetayush2016@users.noreply.github.com>
2023-07-05 19:44:54 +0530

    Update rightAngle_Triangle.java

--- pattern/rightAngle_Triangle.java
+++ pattern/rightAngle_Triangle.java
@@ -44,3 +44,27 @@
 // * * * 
 // * * 
 // * 
+
+ import java.util.*;
+ 
+ class Patters{
+     public static void main(String[] args){
+         int n = 5;
+    
+         for(int i=0;i<=n;i++){
+             for(int j=0;j<=5;j++){
+                 System.out.print("*"); 
+             }
+             System.out.println();
+         }
+         
+     }     
+ }
+
+OUTPUT
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
