87eabe903967620c94a4f78d611bbccd4b32c4a7
Ayush Sharma <83689656+meetayush2016@users.noreply.github.com>
2023-07-14 21:32:14 +0530

    Create removeA_withReturn.java

---
+++ removeA_withReturn.java
@@ -0,0 +1,21 @@
+import java.util.*; 
+
+class Hello{
+    public static void main(String[] args){
+        // System.out.print("hello ");
+        System.out.print(skip("baccad"));
+    }
+    
+    static String skip( String up){
+        if(up.isEmpty()){
+            
+            return "" ; 
+        }
+        char ch = up.charAt(0); 
+        if(ch == 'a'){
+            return skip( up.substring(1)); 
+        }else{
+            return ch+skip(up.substring(1));
+        }
+    }
+}
